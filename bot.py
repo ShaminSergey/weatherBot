6301fbbeeddb884bf11480d2e26abf426dba0a1a
mrDem52 <93878095+mrDem52@users.noreply.github.com>
2025-07-16 23:08:56 +0300

    second + 1

--- bot.py
+++ bot.py
@@ -1,23 +1,55 @@
 import telebot
 from telebot import types
 import webbrowser
+import sqlite3
 
 bot = telebot.TeleBot('7831683127:AAGGh6By1hTbDbvgsAYiCSx-ORVnwVQKp1M')
+name = None
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('Перейти на сайт')  # Создание кнопки
-    markup.row(btn1)
-    btn2 = types.KeyboardButton('Удалить фото')  # Создание кнопки
-    btn3 = types.KeyboardButton('Изменить фото')  # Создание кнопки
-    markup.row(btn2, btn3)
-    file = open('./photo.png', 'rb')
-    bot.send_photo(message.chat.id, file, reply_markup=markup)
-    # bot.reply_to(message, 'Неплохое фото!', reply_markup=markup)
-    bot.send_message(message.chat.id, 'Привет', reply_markup=markup)
-    bot.register_next_step_handler(message, on_click)
+    conn = sqlite3.connect('db.sql')
+    cur = conn.cursor()
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), pass varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close()
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегистрируем! Введи свое имя')
+    bot.register_next_step_handler(message, user_name)
+    # markup = types.ReplyKeyboardMarkup()
+    # btn1 = types.KeyboardButton('Перейти на сайт')  # Создание кнопки
+    # markup.row(btn1)
+    # btn2 = types.KeyboardButton('Удалить фото')  # Создание кнопки
+    # btn3 = types.KeyboardButton('Изменить фото')  # Создание кнопки
+    # markup.row(btn2, btn3)
+    # file = open('./photo.png', 'rb')
+    # bot.send_photo(message.chat.id, file, reply_markup=markup)
+    # # bot.reply_to(message, 'Неплохое фото!', reply_markup=markup)
+    # bot.send_message(message.chat.id, 'Привет', reply_markup=markup)
+    # bot.register_next_step_handler(message, on_click)
+
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введите пароль')
+    bot.register_next_step_handler(message, user_pass)
+
+
+def user_pass(message):
+    password = message.text.strip()
+    conn = sqlite3.connect('db.sql')
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO users (name, pass) VALUES ('%s', '%s')" % (name, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Список пользователей', callback_data='list'))
+    bot.send_message(message.chat.id, 'Пользователь зарегистрирован', reply_markup=markup)
 
 
 def on_click(message):
@@ -62,6 +94,21 @@ def info(message):
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
     elif message.text.lower() == 'id':
         bot.reply_to(message, f'ID: {message.from_user.id}')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    conn = sqlite3.connect('db.sql')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+    info = ''
+    for el in users:
+        info += f'Имя: {el[1]}, пароль: {el[2]}\n'
+
+    cur.close()
+    conn.close()
+    bot.send_message(call.message.chat.id, info)
 
 
 bot.polling(none_stop=True)
